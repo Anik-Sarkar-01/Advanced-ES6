937c8b70110ce44741c468d714b710bf2e6a1932
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2022-10-07 08:10:09 +0530

    Initial commit

---
+++ destructuring-rest-01.js
@@ -0,0 +1,13 @@
+// Here ...c will contain all the values as an object except the value of x,y,z
+const {x,y,z,...c} = {x:45, y:12, z:33, name:'Shakib Al Hasan', Salary:'450'};
+console.log(c);
+
+// ... => is called rest which allows a function to accept indefinite Node. of arguments as an array
+function sum (... theArgs){
+    let total = 0;
+    for(const arg of theArgs){
+        total += arg;
+    }
+    return total;
+}
+console.log(sum(1,2,3));
